f51def8210291d6bec8f8ee0ea2f9a605a1042b3
Liza Maslovskaya <maselnp@gmail.com>
2020-07-10 14:52:41 +0300

    Update main.js

--- 3dGlo/main.js
+++ 3dGlo/main.js
@@ -1,115 +1,144 @@
-window.addEventListener('DOMContentLoaded', function () {
+
   'use strict';
 
-  // таймер 
-  function countTimer(deadline) {
-    let timerHours = document.getElementById('timer-hours'),
-      timerMinutes = document.getElementById('timer-minutes'),
-      timerSeconds = document.getElementById('timer-seconds');
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('menu');
+        // menuItem = menu.querySelectorAll('ul>li'),
+        // startScrollBtn = document.querySelector('.start-scroll-btn');
 
-    function getTimeRemaining() {
-      let updateNum = (num) => String(num).length > 1 ? num : '0' + num;
+    //таймер
+    const countTimer = (deadline) => {
+        const timerHours = document.querySelector('#timer-hours'),
+            timerMinute = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-      let dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = updateNum(Math.floor(timeRemaining % 60)),
-        minutes = updateNum(Math.floor((timeRemaining / 60) % 60)),
-        hours = updateNum(Math.floor(timeRemaining / 60 / 60));
+        const getTimeRemainig = () => {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemainig = (dateStop - dateNow) / 1000,
+                sec = Math.floor(timeRemainig % 60),
+                minutes = Math.floor((timeRemainig / 60) % 60),
+                hours = Math.floor(timeRemainig / 60 / 60);
 
-      return {
-        timeRemaining,
-        hours,
-        minutes,
-        seconds
-      };
-    }
-
-    function updateClock() {
-      let timer = getTimeRemaining();
-
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
-
-      if (timer.timeRemaining < 0) {
-        timerHours.textContent = '00';
-        timerMinutes.textContent = '00';
-        timerSeconds.textContent = '00';
-        clearInterval(interval);
-      }
-    }
-    let interval = setInterval(() => updateClock());
-  }
-  countTimer('11 july 2020');
-
-
-  // меню
-  const toggleMenu = () => {
-    const btnMenu = document.querySelector('.menu'),
-      menu = document.querySelector('menu'),
-      closeBtn = document.querySelector('.close-btn'),
-      menuItems = menu.querySelectorAll('ul>li');
-
-    const handlerMenu = () => {
-      let countShow = 0,
-        countHide = 100,
-        showMenu = function () {
-          if (document.body.clientWidth > 576) {
-            countShow += 2;
-            menu.style.left = countShow + '%';
-            if (countShow < 100) {
-              setTimeout(showMenu, 0);
-            }
-          } else {
-            menu.style.left = 100 + '%';
-          }
-        },
-        hideMenu = function () {
-          if (document.body.clientWidth > 576) {
-            countHide -= 2;
-            menu.style.left = countHide + '%';
-            if (countHide > 0) {
-              setTimeout(hideMenu, 0);
-            }
-          } else {
-            menu.style.left = 0 + '%';
-          }
+            return { timeRemainig, hours, minutes, sec };
         };
 
-      if (!menu.style.left || menu.style.left === '0%') {
-        showMenu();
-      } else {
-        hideMenu();
-      }
+        const updateClock = () => {
+            let timer = getTimeRemainig();
+            timerHours.textContent = timer.hours >= 10 ? timer.hours : '0' + timer.hours;
+            timerMinute.textContent = timer.minutes >= 10 ? timer.minutes : '0' + timer.minutes;
+            timerSeconds.textContent = timer.sec >= 10 ? timer.sec : '0' + timer.sec;
+
+            if(timer.timeRemainig <= 0) {
+                timerHours.textContent = '00';
+                timerMinute.textContent = '00';
+                timerSeconds.textContent = '00';
+            }
+        };
+        updateClock();
     };
+    setInterval(countTimer, 1000, '11 july 2020');
 
-    btnMenu.addEventListener('click', handlerMenu);
-    closeBtn.addEventListener('click', handlerMenu);
-    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
-  };
-  toggleMenu();
+    //меню
+    const toggleMenu = () => {
+        const heandlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
 
+        document.body.addEventListener('click', (event) => {
+            let target = event.target,
+                targetBtnMenu = target.closest('.menu'),
+                targetCloseMenu = target.closest('.close-btn'),
+                targetMenuItem = target.closest('menu>ul>li'),
+                targetMenu = target.closest('.active-menu');
+            
+            if(!targetMenu && !targetBtnMenu && !targetCloseMenu && !targetMenuItem) {
+                menu.classList.remove('active-menu');
+            }
 
-  // попап
-  const toglePopUp = () => {
-    const popup = document.querySelector('.popup'),
-      popupBtns = document.querySelectorAll('.popup-btn'),
-      popupClose = document.querySelector('.popup-close');
+            if(targetBtnMenu || targetCloseMenu || targetMenuItem) {
+                heandlerMenu();
+            } 
+        });
+    };
+    toggleMenu();
 
-    popupBtns.forEach((elem) => {
-      elem.addEventListener('click', () => {
-        popup.style.display = 'block';
-      });
-    });
-    popupBtn.forEach(elem => {
-      elem.addEventListener('click', () => anim = requestAnimationFrame(popupAnim));
-    });
-    popUpClose.addEventListener('click', () => anim = requestAnimationFrame(popupAnim));
+    //попап
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close'),
+            popupContent = document.querySelector('.popup-content');
 
+        let count = 0;
+        let height = popupContent.clientHeight;
+            
+        const popupAnim = () => {
+            count++;
+            popup.style.display = 'block';
+            popupContent.style.top = count + '%';
+            let rafId = requestAnimationFrame(popupAnim);
+            if(count >= 30) {
+                cancelAnimationFrame(rafId);
+            }
+        };
 
-    popupClose.addEventListener('click', () => popup.style.display = 'none');
-  };
-  toglePopUp();
-});
+        const closePopup = () => {
+            count = 0;
+            popupContent.style.top = -height + 'px';
+            popup.style.display = '';
+        };
 
+        popupBtn.forEach( item => {
+            item.addEventListener('click', () => {
+                if(window.innerWidth > 768) {
+                    popupAnim();
+                } else {
+                    popup.style.display = 'block';
+                    popupContent.style.top = '89px';
+                }
+            });
+        });
+        popupClose.addEventListener('click', closePopup);
+
+        popup.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.popup-content');
+            if(!target) {
+                closePopup();
+            }
+        });
+    };
+    togglePopup();
+
+    // скролл
+    // const scrollPage = () => {
+    //     const blocksArray = [serviceBlock, portfolio, calc, command, connect];
+
+    //     const scrollTo = (item) => {
+    //         window.scroll({
+    //             left: 0,
+    //             top: item.offsetTop,
+    //             behavior: 'smooth'
+    //         });
+    //     };
+        
+        
+    //     menuItems.forEach(elem => {
+    //       elem.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         const block = document.querySelector(elem.querySelector('a')
+    //           .getAttribute('href'));
+    //         block.scrollIntoView({ behavior: "smooth", block: "start" });
+    //         toggleMenu();
+    //       });
+    //     });
+
+    //     startScrollBtn.addEventListener('click',(event) => {
+    //       document.querySelector(scrollbtn.getAttribute('href'))
+    //       .scrollIntoView({ behavior: "smooth", block: "start" });    
+    //     });
+    // };
+    // scrollPage();
+
+  });
